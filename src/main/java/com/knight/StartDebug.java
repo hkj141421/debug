3451664537565ab501f8543f726d5603487b4557
knight <670378784@qq.com>
2021-05-31 11:39:36 +0800

    初始化项目

---
+++ src/main/java/com/knight/StartDebug.java
@@ -0,0 +1,13 @@
+package com.knight;
+
+/**
+ * 这是工程启动类
+ * 开始你的DEBUG之旅
+ */
+public class StartDebug
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
